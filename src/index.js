9b2ed4f89943614f5cd647a4ec2f045007d1b7f5
Dayna Leal <dayna.r.leal@gmail.com>
2020-01-23 11:23:27 -0600

    table working now

--- src/index.js
+++ src/index.js
@@ -15,22 +15,10 @@ getMovies().then((movies) => {
   console.log('Here are all the movies:');
   movies.forEach(({title, rating, id}) => {
     console.log(`id#${id} - ${title} - rating: ${rating}`);
-      // $.ajax("db.json")
-      //     .done(function(data){
-      //         console.log(data);
-      for (var i = 0; i < movies.length; i++){
-                  $('#insertProducts').html('<tr>' +
-                      '<td scope="row">' + title + '</td>' +
-                      '<td>' + rating + '</td>' +
-
-                      // '<td>' + '$' + data.price + '</td>' +
-                      // '<td>' + data[i].categories + '</td>' +
-                      '</tr>)');
-              }
-          //
-          //
-          // });
-
+                  $('#insertProducts').append(`<tr>
+                      <td scope="row"> ${title} </td>
+                      <td> ${rating} </td>
+                      </tr>`);
   });
 }).catch((error) => {
   alert('Oh no! Something went wrong.\nCheck the console for details.');
